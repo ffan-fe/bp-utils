1cc86a1d6b722ec6583912a59fef24120b5c0ead
xiaoshude <fzyde123@gmail.com>
2016-08-03 16:00:06 +0800

    修改导航和datepicker示例

--- examples/components/datepicker/controller.js
+++ examples/components/datepicker/controller.js
@@ -1,9 +1,10 @@
 /**
  * Created by fjywan on 16/6/7.
  */
+import Prism from 'prismjs';
 
 class Controller {
-  constructor($scope) {
+  constructor($scope, $sce) {
     'ngInject'
     this.input = 1;
     this.time = 2;
@@ -17,6 +18,26 @@ class Controller {
       viewFormat: 'YYYY-MM-DD HH:mm:ss',
       modelFormat: 'x'
     };
+
+    const exampleCode = `
+      <bp-datepicker ng-model="vm.time" datepicker-options="vm.datepickerOption"></bp-datepicker>
+
+      this.datepickerOption = {
+            minDate: new Date(),
+            viewFormat: 'YYYY-MM-DD HH:mm:ss',
+            modelFormat: 'x'
+          };
+      <bp-rangepicker ng-start-model="vm.startTime"
+                ng-end-model="vm.endTime"
+                rangepicker-options="vm.rangepickerOption">
+      </bp-rangepicker>
+
+      this.rangepickerOption = {
+            viewFormat: 'YYYY-MM-DD HH:mm:ss',
+            modelFormat: 'x'
+          };
+    `;
+    this.exampleHtml = $sce.trustAsHtml(Prism.highlight(exampleCode, Prism.languages.html));
   }
 }
 
