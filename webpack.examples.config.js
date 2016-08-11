f1548ebb84c732f94fb1ed6d1806a72451c6c3c8
xiaoshude <fzyde123@gmail.com>
2016-08-11 14:36:28 +0800

    添加 bpFieldError bpLargeThan

--- webpack.examples.config.js
+++ webpack.examples.config.js
@@ -27,6 +27,11 @@ module.exports = {
     ]
   },
   plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      mangle: {
+        except: ['$super', '$', 'exports', 'require', 'angular']
+      }
+    }),
     new webpack.DefinePlugin({
       'process.env': {
         'DEBUG': true
