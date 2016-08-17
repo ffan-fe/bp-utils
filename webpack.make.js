fa34b31a59936dc0d0dd739c9838b8cd47ce6d4b
xiaoshude <fzyde123@gmail.com>
2016-08-17 11:01:17 +0800

    更新webpack config

---
+++ webpack.make.js
@@ -0,0 +1,225 @@
+'use strict';
+
+// Modules
+var webpack = require('webpack');
+var autoprefixer = require('autoprefixer');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = function makeWebpackConfig(options) {
+  /**
+   * Environment type
+   * BUILD is for generating minified builds
+   * TEST is for generating test builds
+   */
+  var BUILD = !!options.BUILD;
+  var TEST = !!options.TEST;
+  var SRC = !!options.SRC;
+
+  /**
+   * Config
+   * Reference: http://webpack.github.io/docs/configuration.html
+   * This is the object where all configuration gets set
+   */
+  var config = {};
+
+  /**
+   * Entry
+   * Reference: http://webpack.github.io/docs/configuration.html#entry
+   * Should be an empty object if it's generating a test build
+   * Karma will set this when it's a test build
+   */
+  if (TEST) {
+    config.entry = {};
+  } else {
+    config.entry = {
+      "app": './src/index.js'
+    };
+  }
+
+  /**
+   * Output
+   * Reference: http://webpack.github.io/docs/configuration.html#output
+   * Should be an empty object if it's generating a test build
+   * Karma will handle setting it up for you when it's a test build
+   */
+  if (TEST) {
+    config.output = {};
+  } else {
+    config.output = {
+      // Absolute output directory
+      path: __dirname,
+      //path: __dirname + '/dist',
+
+      // Output path from the view of the page
+      // Uses webpack-dev-server in development
+      publicPath: BUILD ? '' : '',
+
+      // Filename for entry points
+      // Only adds hash in build mode
+      filename: BUILD ? '[name].js' : '[name].bundle.js',
+      //filename: BUILD ? '[name].[hash].js' : '[name].bundle.js',
+
+      // Filename for non-entry points
+      // Only adds hash in build mode
+      chunkFilename: BUILD ? '[name].js' : '[name].bundle.js'
+      //chunkFilename: BUILD ? '[name].[hash].js' : '[name].bundle.js'
+    };
+  }
+
+  /**
+   * Devtool
+   * Reference: http://webpack.github.io/docs/configuration.html#devtool
+   * Type of sourcemap to use per build type
+   */
+  if (TEST) {
+    config.devtool = 'inline-source-map';
+  } else if (BUILD) {
+    config.devtool = 'source-map';
+  } else {
+    config.devtool = 'eval';
+  }
+
+  /**
+   * Loaders
+   * Reference: http://webpack.github.io/docs/configuration.html#module-loaders
+   * List: http://webpack.github.io/docs/list-of-loaders.html
+   * This handles most of the magic responsible for converting modules
+   */
+
+    // Initialize module
+  config.module = {
+    preLoaders: [],
+    loaders: [{
+      // JS LOADER
+      // Reference: https://github.com/babel/babel-loader
+      // Transpile .js files using babel-loader
+      // Compiles ES6 and ES7 into ES5 code
+      test: /\.jsx?$/,
+      loader: 'ng-annotate!babel?presets[]=es2015',
+      exclude: /(node_modules|bower_components)/
+    },
+      {
+
+        // IMAGE
+        //The url loader works like the file loader,
+        // but can return a Data Url if the file is smaller than a limit.
+        test: /.(gif|jpg|png)$/,
+        loader: 'url-loader'
+      },
+      {
+        // FONT
+        // ASSET LOADER
+        // Reference: https://github.com/webpack/file-loader
+        // Copy png, jpg, jpeg, gif, svg, woff, woff2, ttf, eot files to output
+        // Rename the file using the asset hash
+        // Pass along the updated reference to your code
+        // You can add here any file extension you want to get copied to your output
+        test: /\.(svg|woff|woff2|ttf|eot)$/,
+        loader: 'file?name=[name].[ext]'
+      },
+      {
+        // HTML LOADER
+        // Reference: https://github.com/webpack/raw-loader
+        // Allow loading html through js
+        test: /\.html$/,
+        loader: 'raw'
+      },
+      {test: /\.less$/, loader: 'style!css!less'},
+      {test: /\.css$/, loader: 'style!css'}]
+  };
+
+  // ISPARTA LOADER
+  // Reference: https://github.com/ColCh/isparta-instrumenter-loader
+  // Instrument JS files with Isparta for subsequent code coverage reporting
+  // Skips node_modules and files that end with .test.js
+  if (TEST) {
+    config.module.preLoaders.push({
+      test: /\.js$/,
+      exclude: [
+        /node_modules/,
+        /\.test\.js$/
+      ],
+      loader: 'isparta-instrumenter'
+    });
+  }
+
+  /**
+   * PostCSS
+   * Reference: https://github.com/postcss/autoprefixer-core
+   * Add vendor prefixes to your css
+   */
+  config.postcss = [
+    autoprefixer({
+      browsers: ['last 2 version']
+    })
+  ];
+
+  /**
+   * Plugins
+   * Reference: http://webpack.github.io/docs/configuration.html#plugins
+   * List: http://webpack.github.io/docs/list-of-plugins.html
+   */
+  config.plugins = [
+    // Reference: https://github.com/webpack/extract-text-webpack-plugin
+    // Extract css files
+    // Disabled when in test mode or not in build mode
+    new ExtractTextPlugin('[name].css', {
+      disable: !BUILD || TEST
+    })
+
+    /*
+     new ExtractTextPlugin('[name].[hash].css', {
+     disable: !BUILD || TEST
+     })
+     */
+  ];
+
+  // Skip rendering index.html in test mode
+  if (!TEST && !SRC) {
+    // Reference: https://github.com/ampedandwired/html-webpack-plugin
+    // Render index.html
+    // Fix bug  Cannot assign to read only property 'canCollapseWhitespace' of true.  https://github.com/ampedandwired/html-webpack-plugin/issues/97
+    config.plugins.push(
+      new HtmlWebpackPlugin({
+        template: './examples/index.html',
+        inject: 'body',
+        minify: false
+      })
+    );
+  }
+
+  // Add build specific plugins
+  if (BUILD) {
+    config.plugins.push(
+      // Reference: http://webpack.github.io/docs/list-of-plugins.html#noerrorsplugin
+      // Only emit files when there are no errors
+      new webpack.NoErrorsPlugin(),
+
+      // Reference: http://webpack.github.io/docs/list-of-plugins.html#dedupeplugin
+      // Dedupe modules in the output
+      new webpack.optimize.DedupePlugin(),
+
+      // Reference: http://webpack.github.io/docs/list-of-plugins.html#uglifyjsplugin
+      // Minify all javascript, switch loaders to minimizing mode
+      new webpack.optimize.UglifyJsPlugin()
+    );
+  }
+
+  /**
+   * Dev server configuration
+   * Reference: http://webpack.github.io/docs/configuration.html#devserver
+   * Reference: http://webpack.github.io/docs/webpack-dev-server.html
+   */
+  config.devServer = {
+    contentBase: './public',
+    stats: {
+      modules: false,
+      cached: false,
+      colors: true,
+      chunk: false
+    }
+  };
+
+  return config;
+};
